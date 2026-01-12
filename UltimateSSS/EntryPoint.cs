f910cfd4a645a05357e74355e13c5ece4010fba5
MSaska <martin.saska@student.sps-cl.cz>
2026-01-12 22:59:04 +0100

    Version bump

--- UltimateSSS/EntryPoint.cs
+++ UltimateSSS/EntryPoint.cs
@@ -15,7 +15,7 @@ public class EntryPoint : Plugin<Config>
     public override string Name { get; } = "UltimateSSS";
     public override string Description { get; } = "Ultimate SSS";
     public override string Author { get; } = "Saskyc";
-    public override Version Version { get; } = new Version(1, 1, 0);
+    public override Version Version { get; } = new Version(1, 2, 0);
     public override Version RequiredApiVersion { get; } = new Version(1, 1, 4);
     public static EntryPoint Instance { get; set; }
     
